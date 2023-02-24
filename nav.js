8173fd6f22b69a78a43853c0b8c7eef2274137ec
lyyxyq <122669652+lyyxyq@users.noreply.github.com>
2023-02-24 10:55:20 +0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,50 @@
+
+    var sd=document.getElementsByClassName("sd")[0];//获取文字元素
+    sd.onmouseover=function(){
+        var sd_img=document.getElementById("1");//获取图片元素
+        sd_img.src="photos/nav.img/水电红.png";//修改图片src
+    }
+    sd.onmouseout=function(){
+        var sd_img=document.getElementById("1");
+        sd_img.src="photos/nav.img/水电.png";
+    }
+
+    var wx=document.getElementsByClassName("wx")[0];//获取文字元素
+    wx.onmouseover=function(){
+        var wx_img=document.getElementById("3");//获取图片元素
+        wx_img.src="photos/nav.img/维修红.png";//修改图片src
+    }
+    wx.onmouseout=function(){
+        var wx_img=document.getElementById("3");
+        wx_img.src="photos/nav.img/维修.png";
+    }
+
+    var sq=document.getElementsByClassName("sq")[0];//获取文字元素
+    sq.onmouseover=function(){
+        var sq_img=document.getElementById("2");//获取图片元素
+        sq_img.src="photos/nav.img/申请红.png";//修改图片src
+    }
+    sq.onmouseout=function(){
+        var sq_img=document.getElementById("2");
+        sq_img.src="photos/nav.img/申请.png";
+    }
+
+    var ss=document.getElementsByClassName("ss")[0];//获取文字元素
+    ss.onmouseover=function(){
+        var ss_img=document.getElementById("4");//获取图片元素
+        ss_img.src="photos/nav.img/宿舍管理红.png";//修改图片src
+    }
+    ss.onmouseout=function(){
+        var ss_img=document.getElementById("4");
+        ss_img.src="photos/nav.img/宿舍管理.png";
+    }
+
+    var sg=document.getElementsByClassName("sg")[0];//获取文字元素
+    sg.onmouseover=function(){
+        var sg_img=document.getElementById("5");//获取图片元素
+        sg_img.src="photos/nav.img/生管红.png";//修改图片src
+    }
+    sg.onmouseout=function(){
+        var sg_img=document.getElementById("5");
+        sg_img.src="photos/nav.img/生管.png";
+    }
